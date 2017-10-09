41f4b826d2d5476bda211d5be06f0b8a5f17fe5c
Jedsda Tiwongvorakul <jt@20scoops.net>
2017-10-09 16:01:23 +0700

    update code,.

--- src/Calculator.php
+++ src/Calculator.php
@@ -1,25 +1,25 @@
 <?php
 
-class Calculator
-{
-    function plus($x, $y)
-    {
-        return $x+$y;
+class Calculator {
+    function plus($a,$b){
+        return $a+$b;
     }
 
-    function minus($x , $y)
-    {
-        return $x - $y;
+    function minus($a,$b){
+        return $a-$b;
     }
 
-    public function multi($x, $y)
-    {
-        return $x * $y;
+    function multiply($a,$b){
+        return $a*$b;
     }
 
-    public function divide($x, $y)
-    {
-        if($y == 0) return 0;
-        else return $x/$y;
+    function divide($a,$b){
+        if($b == 0) {
+            return 0;
+        } else if($a === null || $b === null) {
+            return "Please Input Number";
+        } else {
+            return $a/$b;
+        }
     }
 }
--- test/CalculatorTest.php
+++ test/CalculatorTest.php
@@ -3,29 +3,45 @@
 require "src/Calculator.php";
 
 class CalculatorTest extends \PHPUnit_Framework_TestCase {
-
-    public function testPlus() {
-        $calculator = new Calculator;
-        $this->assertEquals(2, $calculator->plus(1, 1));
-    }
-    
-    public function testMinus() {
-        $calculator = new Calculator;
-        $this->assertEquals(0, $calculator->minus(1, 1));
-    }
-    
-    public function testMulti() {
-        $calculator = new Calculator;
-        $this->assertEquals(1, $calculator->multi(1, 1));
+    public function test10plus10()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(20,$calculator->plus(10,10));
     }
 
-    public function testDivide() {
-        $calculator = new Calculator;
-        $this->assertEquals(1, $calculator->divide(1, 1));
+    public function test10minus10()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(0,$calculator->minus(10,10));
     }
 
-    public function testDivideByZero() {
-        $calculator = new Calculator;
-        $this->assertEquals(0, $calculator->divide(1, 0));
+    public function test10multiply10()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(100,$calculator->multiply(10,10));
+    }
+
+    public function test10divide10()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(1,$calculator->divide(10,10));
+    }
+
+    public function testDivideByZero()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(0,$calculator->divide(10,0));
+    }
+
+    public function testNullDivide10()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals("Please Input Number",$calculator->divide(null,10));
+    }
+
+    public function testZeroDivideNumber()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(0,$calculator->divide(0,10));
     }
 }
