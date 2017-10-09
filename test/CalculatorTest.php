e44b12f69220c7487b668c2d6da159563b1059a0
Jedsda Tiwongvorakul <jt@20scoops.net>
2017-10-09 16:17:52 +0700

    update code,.

--- test/CalculatorTest.php
+++ test/CalculatorTest.php
@@ -3,45 +3,46 @@
 require "src/Calculator.php";
 
 class CalculatorTest extends \PHPUnit_Framework_TestCase {
+
+    private $calculator;
+
+    protected function setUp()
+    {
+        $this->calculator = new Calculator();
+    }
+
     public function test10plus10()
     {
-        $calculator = new Calculator();
-        $this->assertEquals(20,$calculator->plus(10,10));
+        $this->assertEquals(20,$this->calculator->plus(10,10));
     }
 
     public function test10minus10()
     {
-        $calculator = new Calculator();
-        $this->assertEquals(0,$calculator->minus(10,10));
+        $this->assertEquals(0,$this->calculator->minus(10,10));
     }
 
     public function test10multiply10()
     {
-        $calculator = new Calculator();
-        $this->assertEquals(100,$calculator->multiply(10,10));
+        $this->assertEquals(100,$this->calculator->multiply(10,10));
     }
 
     public function test10divide10()
     {
-        $calculator = new Calculator();
-        $this->assertEquals(1,$calculator->divide(10,10));
+        $this->assertEquals(1,$this->calculator->divide(10,10));
     }
 
     public function testDivideByZero()
     {
-        $calculator = new Calculator();
-        $this->assertEquals(0,$calculator->divide(10,0));
+        $this->assertEquals(0,$this->calculator->divide(10,0));
     }
 
     public function testNullDivide10()
     {
-        $calculator = new Calculator();
-        $this->assertEquals("Please Input Number",$calculator->divide(null,10));
+        $this->assertEquals("Please Input Number",$this->calculator->divide(null,10));
     }
 
     public function testZeroDivideNumber()
     {
-        $calculator = new Calculator();
-        $this->assertEquals(0,$calculator->divide(0,10));
+        $this->assertEquals(0,$this->calculator->divide(0,10));
     }
 }
